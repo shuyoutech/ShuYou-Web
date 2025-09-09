95f34b87cc719acffb05750ff2e0ffe889808c7c
YangChao <1219191915@qq.com>
2025-09-09 19:32:31 +0800

    add:init project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,39 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
-    devtools: {enabled: true},
+  compatibilityDate: '2025-07-15',
+  devtools: {enabled: true},
 
-    modules: [
-        '@nuxt/content',
-        '@nuxt/eslint',
-        '@nuxt/image',
-        '@nuxt/scripts',
-        '@nuxt/test-utils',
-        '@nuxt/ui',
-        'nuxt-og-image'
-    ],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    'nuxt-og-image'
+  ],
 
-    css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
 
-    routeRules: {
-        '/docs': {redirect: '/docs/getting-started', prerender: false}
-    },
+  routeRules: {
+    '/docs': {redirect: '/docs/getting-started', prerender: false}
+  },
 
-    nitro: {
-        prerender: {
-            routes: [
-                '/'
-            ],
-            crawlLinks: true
-        }
-    },
-
-    eslint: {
-        config: {
-            stylistic: {
-                commaDangle: 'never',
-                braceStyle: '1tbs'
-            }
-        }
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
     }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
